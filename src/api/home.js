4f76d612d788acc71f25d4d5806e73c2bb9bb5d5
funnyCodingDevil <sjh201411@gmail.com>
2022-10-06 01:17:57 +0800

    完成首页轮播图

--- src/api/home.js
+++ src/api/home.js
@@ -1,5 +1,16 @@
 import request from '@/utils/request'
-
+/**
+ * 获取品牌
+ * @params {}
+ * @returns Promise
+ */
 export const findBrand = (limit = 6) => {
   return request('/home/brand', 'GET', { limit })
+}
+/**
+ * 获取广告图
+ * @returns Promise
+ */
+export const findBanner = () => {
+  return request('/home/banner', 'get')
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="container">
-    <div class="home-entry">
+  <!-- 首页入口 -->
+  <div class="home-entry">
+    <div class="container">
       <!-- 左侧分类 -->
       <HomeCategory />
       <!-- 轮播图 -->
+      <HomeBanner />
     </div>
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'Home',
-  components: { HomeCategory }
+  components: {
+    HomeCategory,
+    HomeBanner
+  }
 }
 </script>
 
