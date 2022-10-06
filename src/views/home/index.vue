3e14a2c447ad150768a181020524f3e4c55a74d5
funnyCodingDevil <sjh201411@gmail.com>
2022-10-07 01:09:53 +0800

    完成人气推荐和新鲜好物

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,17 +9,23 @@
     </div>
   </div>
   <!-- 新鲜好物 -->
+  <HomeNew />
   <!-- 人气推荐 -->
+  <HomeHot />
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
 import HomeBanner from './components/home-banner.vue'
+import HomeNew from './components/home-new.vue'
+import HomeHot from './components/home-hot.vue'
 export default {
   name: 'Home',
   components: {
     HomeCategory,
-    HomeBanner
+    HomeBanner,
+    HomeNew,
+    HomeHot
   }
 }
 </script>
